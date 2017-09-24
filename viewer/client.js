50350664036b075f996adea0db3092a64e7e1e14
CM Lubinski <christopher.lubinski@gsa.gov>
2017-09-24 13:47:17 -0400

    Add query caching.

--- viewer/client.js
+++ viewer/client.js
@@ -1,3 +1,27 @@
-import axios from 'axios';
+import { stringify } from 'querystring';
 
-export default axios.create({ baseURL: `${process.env.API_BASE}akn/us` });
+import axios from 'axios';
+import LRU from 'lru-cache';
+
+class Client {
+  constructor(baseURL) {
+    this.httpClient = axios.create({ baseURL });
+    this.cache = LRU({
+      max: 50,
+      maxAge: 1000 * 60 * 5, // 5 minutes
+    });
+  }
+
+  async get(url, params) {
+    const cacheKey = `${url}?${stringify(params || {})}`;
+    if (this.cache.has(cacheKey)) {
+      return this.cache.get(cacheKey);
+    }
+
+    const { data } = await this.httpClient.get(url, { params });
+    this.cache.set(cacheKey, data);
+    return data;
+  }
+}
+
+export default new Client(`${process.env.API_BASE}akn/us`);
