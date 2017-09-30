177b4d898f995e88e9307bda10fb1d775ec2d686
CM Lubinski <christopher.lubinski@gsa.gov>
2017-09-29 20:16:44 -0400

    Fixup max_depth.

--- api/docular/structure/serializers.py
+++ api/docular/structure/serializers.py
@@ -51,22 +51,16 @@ class CursorSerializer(ModelSerializer):
         struct.cursor = instance
         return super().to_representation(struct)
 
-    def child_context(self):
-        context = dict(self.context)    # shallow copy
-        if 'max_depth' in context:
-            context['max_depth'] = context['max_depth'] - 1
-        elif 'request' in context:
-            max_depth = context['request'].GET.get('max_depth', '')
-            if max_depth.isnumeric():
-                context['max_depth'] = int(max_depth) - 1
-        return context
-
     def get_children(self, instance):
-        context = self.child_context()
-        if context.get('max_depth', 0) < 0:
+        context = dict(self.context)    # shallow copy
+        context['depth'] = context.get('depth', -1) + 1
+        max_depth = ''
+        if 'request' in context:
+            max_depth = context['request'].GET.get('max_depth', '')
+        if max_depth.isnumeric() and int(max_depth) <= context['depth']:
             return []
         return self.__class__(
-            instance.cursor.children(), context=self.context, many=True
+            instance.cursor.children(), context=context, many=True
         ).data
 
     def get_meta(self, instance):
@@ -76,23 +70,16 @@ class CursorSerializer(ModelSerializer):
 class RootSerializer(CursorSerializer):
     def get_meta(self, instance):
         meta = super().get_meta(instance)
-        expression = ExpressionSerializer().to_representation(
-            instance.expression)
-        meta.update(
-            prev_doc=NavSerializer.or_none(instance.previous()),
-            prev_peer=NavSerializer.or_none(instance.prev_peer()),
-            next_doc=NavSerializer.or_none(instance.following()),
-            next_peer=NavSerializer.or_none(instance.next_peer()),
-            parent_doc=NavSerializer.or_none(instance.parent()),
-            frbr={'work': expression.pop('work'),
-                  'expression': expression},
-        )
+        if 'depth' not in self.context:
+            expression = ExpressionSerializer().to_representation(
+                instance.expression)
+            meta.update(
+                prev_doc=NavSerializer.or_none(instance.previous()),
+                prev_peer=NavSerializer.or_none(instance.prev_peer()),
+                next_doc=NavSerializer.or_none(instance.following()),
+                next_peer=NavSerializer.or_none(instance.next_peer()),
+                parent_doc=NavSerializer.or_none(instance.parent()),
+                frbr={'work': expression.pop('work'),
+                      'expression': expression},
+            )
         return meta
-
-    def get_children(self, instance):
-        context = self.child_context()
-        if context.get('max_depth', 0) < 0:
-            return []
-        return CursorSerializer(
-            instance.cursor.children(), context=self.context, many=True
-        ).data
